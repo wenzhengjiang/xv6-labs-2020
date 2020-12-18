e80f1a57c5b10fed97aefe2a96921e13a8fe6493
Wenzheng Jiang <wenzheng@wenzhengjiang.com>
2020-12-18 14:34:35 +0900

    Traps: backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,14 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void backtrace() {
+  uint64 fp = r_fp();
+  uint64 stack_top = PGROUNDUP(fp);
+
+  printf("backtrace:\n");
+  while(fp < stack_top) {
+    printf("%p\n", *(uint64 *)(fp-8));
+    fp = *(uint64 *)(fp-16);
+  };
+}
