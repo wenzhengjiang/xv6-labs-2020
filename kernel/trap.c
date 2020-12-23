53e78397264abd0ac8aa85819992bd9e0a0a5355
Wenzheng Jiang <wenzheng@wenzhengjiang.com>
2020-12-23 12:21:35 +0900

    cow: Modified traps so the store page fault on copy-on-write page get handled, and met the problem that freeproc freed pages what are still used by parent process

--- kernel/trap.c
+++ kernel/trap.c
@@ -29,6 +29,29 @@ trapinithart(void)
   w_stvec((uint64)kernelvec);
 }
 
+int cowfault(pagetable_t pagetable, uint64 va) {
+  if (va >= MAXVA)
+    return -1;
+  pte_t *pte = walk(pagetable, va, 0);
+  if (pte == 0)
+    return -1;
+  if ((*pte & PTE_U) == 0 || (*pte & PTE_V) == 0)
+    return -1;
+
+  uint64 pa1 = PTE2PA(*pte);
+
+  uint64 pa2 = (uint64) kalloc();
+  if (pa2 == 0) {
+    printf("cow kalloc failed\n");
+  }
+
+  memmove((void*)pa2, (void*)pa1, 4096);
+
+  *pte = PA2PTE(pa2) | PTE_V | PTE_U | PTE_R | PTE_W | PTE_X;
+
+  return 0;
+}
+
 //
 // handle an interrupt, exception, or system call from user space.
 // called from trampoline.S
@@ -65,6 +88,10 @@ usertrap(void)
     intr_on();
 
     syscall();
+  } else if(r_scause() == 0xf){
+    if (cowfault(p->pagetable, r_stval()) < 0) {
+      p->killed = 1;
+    }
   } else if((which_dev = devintr()) != 0){
     // ok
   } else {
