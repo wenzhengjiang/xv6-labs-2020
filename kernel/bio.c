1f1f095a22cdefcef30c4bbcfac76486453c5685
Wenzheng Jiang <wenzheng@wenzhengjiang.com>
2020-12-26 21:58:22 +0900

    Remove head list and use timestamp to find lru. This removed the bcache lock in brelse, so the loop iterations for acquiring bcache lock got decreased from 244610 to 196203

--- kernel/bio.c
+++ kernel/bio.c
@@ -26,11 +26,6 @@
 struct {
   struct spinlock lock;
   struct buf buf[NBUF];
-
-  // Linked list of all buffers, through prev/next.
-  // Sorted by how recently the buffer was used.
-  // head.next is most recent, head.prev is least.
-  struct buf head;
 } bcache;
 
 void
@@ -41,14 +36,8 @@ binit(void)
   initlock(&bcache.lock, "bcache");
 
   // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
   for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
     initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
   }
 }
 
@@ -58,12 +47,10 @@ binit(void)
 static struct buf*
 bget(uint dev, uint blockno)
 {
-  struct buf *b;
-
   acquire(&bcache.lock);
 
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  for(struct buf *b = bcache.buf; b < bcache.buf+NBUF; b++){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
       release(&bcache.lock);
@@ -74,16 +61,20 @@ bget(uint dev, uint blockno)
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
-      b->dev = dev;
-      b->blockno = blockno;
-      b->valid = 0;
-      b->refcnt = 1;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
+  struct buf *lru = 0;
+  for(struct buf *b = bcache.buf; b < bcache.buf+NBUF; b++){
+    if (b->refcnt == 0 && (lru == 0 || b->last_time_used < lru->last_time_used)) {
+      lru = b;
     }
+  }
+  if(lru) {
+    lru->dev = dev;
+    lru->blockno = blockno;
+    lru->valid = 0;
+    lru->refcnt = 1;
+    release(&bcache.lock);
+    acquiresleep(&lru->lock);
+    return lru;
   }
   panic("bget: no buffers");
 }
@@ -112,7 +103,6 @@ bwrite(struct buf *b)
 }
 
 // Release a locked buffer.
-// Move to the head of the most-recently-used list.
 void
 brelse(struct buf *b)
 {
@@ -122,16 +112,8 @@ brelse(struct buf *b)
   releasesleep(&b->lock);
 
   acquire(&bcache.lock);
+  b->last_time_used = ticks;
   b->refcnt--;
-  if (b->refcnt == 0) {
-    // no one is waiting for it.
-    b->next->prev = b->prev;
-    b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
-  }
   
   release(&bcache.lock);
 }
